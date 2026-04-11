bce3a1e1858332127a9144dde0f05bb9d93c46df
Valentin Valls <valentin.valls@esrf.fr>
2026-04-11 15:31:04 +0200

    Return binary as memoryview

--- thriftpy2/protocol/cybin2/cybin2.pyx
+++ thriftpy2/protocol/cybin2/cybin2.pyx
@@ -6,6 +6,7 @@ from libc.stdlib cimport free, malloc
 from libc.stdint cimport int16_t, int32_t, int64_t
 from libc.string cimport memcpy
 from cpython cimport bool, PyObject_GetBuffer, PyBuffer_Release, PyBUF_ANY_CONTIGUOUS, PyBUF_SIMPLE
+from cython.cimports.cython.view import array as cvarray
 
 from thriftpy2.thrift import TDecodeException
 from thriftpy2.transport.cybase cimport CyTransportBase, STACK_STRING_LEN
@@ -251,7 +252,7 @@ cdef inline write_struct(CyTransportBase buf, obj):
     write_i08(buf, T_STOP)
 
 
-cdef inline c_read_binary(CyTransportBase buf, int32_t size):
+cdef inline c_read_binary_as_bytes(CyTransportBase buf, int32_t size):
     cdef char string_val[STACK_STRING_LEN]
 
     if size > STACK_STRING_LEN:
@@ -268,9 +269,16 @@ cdef inline c_read_binary(CyTransportBase buf, int32_t size):
     return py_data
 
 
+cdef inline c_read_binary_as_memoryview(CyTransportBase buf, int32_t size):
+    array = cvarray(shape=(size,), itemsize=sizeof(char), format="b")
+    cdef char[:] mview = array
+    buf.c_read(size, &mview[0])
+    return mview
+
+
 cdef inline c_read_string(CyTransportBase buf, int32_t size,
                           strict_decode=False):
-    py_data = c_read_binary(buf, size)
+    py_data = c_read_binary_as_bytes(buf, size)
     try:
         return (<char *>py_data)[:size].decode("utf-8")
     except:  # noqa
@@ -308,14 +316,14 @@ cdef c_read_val(CyTransportBase buf, TType ttype, spec=None,
 
     elif ttype == T_BINARY:
         size = read_i32(buf)
-        return c_read_binary(buf, size)
+        return c_read_binary_as_memoryview(buf, size)
 
     elif ttype == T_STRING:
         size = read_i32(buf)
         if decode_response:
             return c_read_string(buf, size, strict_decode)
         else:
-            return c_read_binary(buf, size)
+            return c_read_binary_as_bytes(buf, size)
 
     elif ttype == T_SET or ttype == T_LIST:
         if isinstance(spec, int):
@@ -432,7 +440,7 @@ cpdef skip(CyTransportBase buf, TType ttype):
         read_i64(buf)
     elif ttype == T_STRING or ttype == T_BINARY:
         size = read_i32(buf)
-        c_read_binary(buf, size)
+        c_read_binary_as_bytes(buf, size)
     elif ttype == T_SET or ttype == T_LIST:
         v_type = <TType>read_i08(buf)
         size = read_i32(buf)
